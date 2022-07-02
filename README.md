440700431ed5cfb54fadede607b4b5901d568450
Ajeesh Joshy <jishcem@gmail.com>
2022-07-02 21:52:03 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+rsync -e "ssh -i ~/jishcem.pem" -av --exclude={'dist','node_modules'} ./ ubuntu@13.233.201.22:/home/ubuntu/Code/typescript-express
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from 'express'
+
+const app = express()
+const port = 3000
+
+app.get('/', (req: Request, res: Response) => {
+    res.send("Hello World")
+})
+
+app.listen(port, () => {
+    console.log(`App listening on the port ${port}`);  
+})
