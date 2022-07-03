806580e98370feac28e042fc0836c7810538f1f8
Ajeesh Joshy <jishcem@gmail.com>
2022-07-03 20:51:02 +0530

    Add user data and actions workflow

--- etc/user-data.sh
+++ etc/user-data.sh
@@ -7,3 +7,17 @@ sudo apt install nginx -y
 sudo apt install certbot python3-certbot-nginx
 
 sudo certbot --nginx -d ajeesh.xyz
+
+
+# To install AWS Code Deploy agent. 
+sudo apt update
+sudo apt install ruby-full
+sudo apt install wget
+
+wget https://aws-codedeploy-ap-south-1.s3.ap-south-1.amazonaws.com/latest/install
+chmod +x ./install
+
+sudo service codedeploy-agent status
+sudo service codedeploy-agent stop
+sudo service codedeploy-agent start
+sudo service codedeploy-agent status
