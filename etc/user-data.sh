b8f9a6bfdd19bc18c3349ea3cc7f5295b3034190
Ajeesh Joshy <jishcem@gmail.com>
2022-07-16 10:36:19 +0530

    Uupdate user data

--- etc/user-data.sh
+++ etc/user-data.sh
@@ -16,6 +16,7 @@ sudo apt install wget
 
 wget https://aws-codedeploy-ap-south-1.s3.ap-south-1.amazonaws.com/latest/install
 chmod +x ./install
+sudo ./install auto > /tmp/logfile
 
 sudo service codedeploy-agent status
 sudo service codedeploy-agent stop
